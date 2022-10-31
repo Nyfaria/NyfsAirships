89eb4707c86afb6fe11f9170916fb65cd5e5360b
Nyfaria DragonMoose <kinaria3324@gmail.com>
2022-10-31 09:24:54 -0500

    fixed controls
    Took 43 minutes

--- src/main/java/com/nyfaria/nyfsairships/ClientInit.java
+++ src/main/java/com/nyfaria/nyfsairships/ClientInit.java
@@ -1,6 +1,7 @@
 package com.nyfaria.nyfsairships;
 
 import com.mojang.blaze3d.platform.InputConstants;
+import com.nyfaria.nyfsairships.entity.BlockShipEntity;
 import com.nyfaria.nyfsairships.entity.BlockShipEntityRenderer;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
@@ -9,6 +10,7 @@ import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.api.client.rendereregistry.v1.EntityRendererRegistry;
 import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 import net.minecraft.client.KeyMapping;
+import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.network.FriendlyByteBuf;
 import org.lwjgl.glfw.GLFW;
 
@@ -23,115 +25,24 @@ public class ClientInit implements ClientModInitializer {
         up = KeyBindingHelper.registerKeyBinding(new KeyMapping("key.nyfsairships.up", InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_Z,"category.nyfsairships.main"));
         down= KeyBindingHelper.registerKeyBinding(new KeyMapping("key.nyfsairships.down", InputConstants.Type.KEYSYM,GLFW.GLFW_KEY_C,"category.nyfsairships.main"));
         stop = KeyBindingHelper.registerKeyBinding(new KeyMapping("key.nyfsairships.stop", InputConstants.Type.KEYSYM,GLFW.GLFW_KEY_V,"category.nyfsairships.main"));
-
         ClientTickEvents.START_CLIENT_TICK.register(client ->
         {
-            while (client.options.keyUp.consumeClick()) {
-                FriendlyByteBuf forw = PacketByteBufs.create();
-                forw.writeInt(0);
-                ClientPlayNetworking.send(NyfsAirships.forwardPacket,forw);
+            if(client.player != null) {
+                LocalPlayer player = client.player;
+                if (player.isPassenger() && player.getVehicle() instanceof BlockShipEntity airShip) {
+                    if (up.isDown()) {
+                        airShip.setYya(0.8f);
+                    } else if (down.isDown()) {
+                        if (!airShip.isOnGround()) {
+                            airShip.setYya(-0.8f);
+                            player.input.shiftKeyDown = false;
+                        }
+                    } else {
+                        airShip.setYya(0);
+                    }
+
+                }
             }
-
-        });
-        ClientTickEvents.START_CLIENT_TICK.register(client ->
-        {
-            while (client.options.keyLeft.consumeClick()) {
-                FriendlyByteBuf forw = PacketByteBufs.create();
-                forw.writeInt(1);
-                ClientPlayNetworking.send(NyfsAirships.forwardPacket,forw);
-            }
-
-        });
-        ClientTickEvents.START_CLIENT_TICK.register(client ->
-        {
-            while (client.options.keyRight.consumeClick()) {
-                FriendlyByteBuf forw = PacketByteBufs.create();
-                forw.writeInt(2);
-                ClientPlayNetworking.send(NyfsAirships.forwardPacket,forw);
-            }
-
-        });
-        ClientTickEvents.START_CLIENT_TICK.register(client ->
-        {
-            while (up.consumeClick()) {
-                FriendlyByteBuf forw = PacketByteBufs.create();
-                forw.writeInt(3);
-                ClientPlayNetworking.send(NyfsAirships.forwardPacket,forw);
-            }
-
-        });
-        ClientTickEvents.START_CLIENT_TICK.register(client ->
-        {
-            while (down.consumeClick()) {
-                FriendlyByteBuf forw = PacketByteBufs.create();
-                forw.writeInt(4);
-                ClientPlayNetworking.send(NyfsAirships.forwardPacket,forw);
-            }
-
-        });
-        ClientTickEvents.END_CLIENT_TICK.register(client ->
-        {
-            while (stop.consumeClick()) {
-                FriendlyByteBuf forw =PacketByteBufs.create();
-                forw.writeInt(5);
-                ClientPlayNetworking.send(NyfsAirships.forwardPacket,forw);
-            }
-
-        });
-
-        ClientTickEvents.END_CLIENT_TICK.register(client ->
-        {
-            while (client.options.keyUp.consumeClick()) {
-                FriendlyByteBuf forw = PacketByteBufs.create();
-                forw.writeInt(0);
-                ClientPlayNetworking.send(NyfsAirships.forwardPacket,forw);
-            }
-
-        });
-        ClientTickEvents.END_CLIENT_TICK.register(client ->
-        {
-            while (client.options.keyLeft.consumeClick()) {
-                FriendlyByteBuf forw = PacketByteBufs.create();
-                forw.writeInt(1);
-                ClientPlayNetworking.send(NyfsAirships.forwardPacket,forw);
-            }
-
-        });
-        ClientTickEvents.END_CLIENT_TICK.register(client ->
-        {
-            while (client.options.keyRight.consumeClick()) {
-                FriendlyByteBuf forw = PacketByteBufs.create();
-                forw.writeInt(2);
-                ClientPlayNetworking.send(NyfsAirships.forwardPacket,forw);
-            }
-
-        });
-        ClientTickEvents.END_CLIENT_TICK.register(client ->
-        {
-            while (up.consumeClick()) {
-                FriendlyByteBuf forw = PacketByteBufs.create();
-                forw.writeInt(3);
-                ClientPlayNetworking.send(NyfsAirships.forwardPacket,forw);
-            }
-
-        });
-        ClientTickEvents.END_CLIENT_TICK.register(client ->
-        {
-            while (down.consumeClick()) {
-                FriendlyByteBuf forw = PacketByteBufs.create();
-                forw.writeInt(4);
-                ClientPlayNetworking.send(NyfsAirships.forwardPacket,forw);
-            }
-
-        });
-        ClientTickEvents.END_CLIENT_TICK.register(client ->
-        {
-            while (stop.consumeClick()) {
-                FriendlyByteBuf forw =PacketByteBufs.create();
-                forw.writeInt(5);
-                ClientPlayNetworking.send(NyfsAirships.forwardPacket,forw);
-            }
-
         });
     }
 }
--- src/main/java/com/nyfaria/nyfsairships/entity/BlockShipEntity.java
+++ src/main/java/com/nyfaria/nyfsairships/entity/BlockShipEntity.java
@@ -2,46 +2,46 @@ package com.nyfaria.nyfsairships.entity;
 
 import com.nyfaria.nyfsairships.ConfigUtils;
 import com.nyfaria.nyfsairships.NyfsAirships;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.Mob;
-import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
-import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.world.entity.LightningBolt;
-import net.minecraft.world.entity.animal.Pig;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.level.material.Fluid;
-import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
+import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.nbt.Tag;
 import net.minecraft.nbt.ListTag;
 import net.minecraft.nbt.StringTag;
-import net.minecraft.tags.FluidTags;
-import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.InteractionResult;
-import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.InteractionHand;
+import net.minecraft.nbt.Tag;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
-import net.minecraft.core.BlockPos;
+import net.minecraft.tags.FluidTags;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LightningBolt;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.PathfinderMob;
+import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.phys.Vec3;
-import net.minecraft.network.chat.TextComponent;
-import net.minecraft.world.level.Level;
-import net.minecraft.server.level.ServerLevel;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.UUID;
 
-public class BlockShipEntity extends Pig {
+public class BlockShipEntity extends PathfinderMob {
     public BlockShipEntity(EntityType<? extends BlockShipEntity> entityType, Level world) {
         super(entityType, world);
     }
-
 
 
     public boolean canStandOnFluid(FluidState p_230285_1_) {
@@ -51,46 +51,27 @@ public class BlockShipEntity extends Pig {
     @Override
     protected void registerGoals() {
     }
+
     public static AttributeSupplier.Builder createAttributes() {
         return Mob.createMobAttributes().add(Attributes.MAX_HEALTH, 10.0D).add(Attributes.MOVEMENT_SPEED, 0.25D);
     }
 
 
-
     @Override
     public float getSpeed() {
-        if(this.getControllingPassenger() instanceof  Player)
-        {
-            if(((Player) this.getControllingPassenger()).getMainHandItem().getItem()== NyfsAirships.CONTROL_KEY_ITEM)
-            {
-                float cspeed = Float.parseFloat(ConfigUtils.config.getOrDefault("cspeed","0.2"));
-                float nspeed = Float.parseFloat(ConfigUtils.config.getOrDefault("nspeed","0.05"));
-                if(this.getItemBySlot(EquipmentSlot.CHEST).getItem()==Items.OAK_PLANKS)
-                {
-                    if(this.getItemBySlot(EquipmentSlot.CHEST).getTag().getInt("type")==0)
-                    {
-                        if(this.level.getBlockState(this.getBlockPosBelowThatAffectsMyMovement()).getBlock()== Blocks.WATER)
-                        {
-                            ListTag go = (ListTag) this.getItemBySlot(EquipmentSlot.CHEST).getTag().get("addons");
-                            if(go.contains(StringTag.valueOf("engine")))
-                            {
-                                return cspeed*1.5f;
-                            }
-                            return cspeed;
-                        }
+        if (this.getControllingPassenger() != null) {
+            if (this.getControllingPassenger().getMainHandItem().is(NyfsAirships.CONTROL_KEY_ITEM) || this.getControllingPassenger().getOffhandItem().is(NyfsAirships.CONTROL_KEY_ITEM)) {
+                float cspeed = Float.parseFloat(ConfigUtils.config.getOrDefault("cspeed", "0.1"));
+                float nspeed = Float.parseFloat(ConfigUtils.config.getOrDefault("nspeed", "0.05"));
+                if (this.getItemBySlot(EquipmentSlot.CHEST).getItem() == Items.OAK_PLANKS) {
+
+                    ListTag go = (ListTag) this.getItemBySlot(EquipmentSlot.CHEST).getTag().get("addons");
+                    if (go.contains(StringTag.valueOf("engine"))) {
+                        cspeed *= 1.5f;
+                    }
+                    if(this.isOnGround()){
                         return nspeed;
-                    }
-                    if(this.getItemBySlot(EquipmentSlot.CHEST).getTag().getInt("type")==1)
-                    {
-                        return 0f;
-                    }
-                    else
-                    {
-                        ListTag go = (ListTag) this.getItemBySlot(EquipmentSlot.CHEST).getTag().get("addons");
-                        if(go.contains(StringTag.valueOf("engine")))
-                        {
-                            return cspeed*1.5f;
-                        }
+                    } else {
                         return cspeed;
                     }
                 }
@@ -116,13 +97,12 @@ public class BlockShipEntity extends Pig {
 
     @Override
     public boolean canBeControlledByRider() {
-        if(this.getItemBySlot(EquipmentSlot.HEAD).getItem()==Items.STICK)
-        {
-            return false;
-        }
+//        if(this.getItemBySlot(EquipmentSlot.HEAD).getItem()==Items.STICK)
+//        {
+//            return false;
+//        }
         return true;
     }
-
 
 
     @Override
@@ -130,25 +110,23 @@ public class BlockShipEntity extends Pig {
         return 0;
     }
 
-    public void setModel(ListTag input, int direction, int offset, int type, CompoundTag storage,ListTag addons)
-    {
+    public void setModel(ListTag input, int direction, int offset, int type, CompoundTag storage, ListTag addons) {
         ItemStack itemStack = new ItemStack(Items.OAK_PLANKS);
         CompoundTag tag = new CompoundTag();
         tag.putString("model", UUID.randomUUID().toString());
-        tag.put("parts",input);
-        tag.putInt("direction",direction);
-        tag.putInt("offset",offset);
-        tag.putInt("type",type);
-        tag.put("storage",storage);
-        tag.put("addons",addons);
+        tag.put("parts", input);
+        tag.putInt("direction", direction);
+        tag.putInt("offset", offset);
+        tag.putInt("type", type);
+        tag.put("storage", storage);
+        tag.put("addons", addons);
         itemStack.setTag(tag);
-        this.setItemSlot(EquipmentSlot.CHEST,itemStack);
+        this.setItemSlot(EquipmentSlot.CHEST, itemStack);
     }
 
     @Override
     public boolean isInvulnerableTo(DamageSource p_180431_1_) {
-        if(p_180431_1_!=DamageSource.OUT_OF_WORLD)
-        {
+        if (p_180431_1_ != DamageSource.OUT_OF_WORLD) {
             return true;
         }
         return false;
@@ -156,25 +134,21 @@ public class BlockShipEntity extends Pig {
 
     @Override
     public boolean isInvulnerable() {
-        if(!dead)
-        {
+        if (!dead) {
             return true;
         }
         return false;
     }
 
-    public void tryDisassemble()
-    {
-        if(this.getItemBySlot(EquipmentSlot.CHEST).getItem()==Items.OAK_PLANKS)
-        {
+    public void tryDisassemble() {
+        if (this.getItemBySlot(EquipmentSlot.CHEST).getItem() == Items.OAK_PLANKS) {
             ListTag list = (ListTag) this.getItemBySlot(EquipmentSlot.CHEST).getTag().get("parts");
             int offset = this.getItemBySlot(EquipmentSlot.CHEST).getTag().getInt("offset");
             CompoundTag storage = this.getItemBySlot(EquipmentSlot.CHEST).getTag().getCompound("storage");
-            for (Tag tag : list)
-            {
+            for (Tag tag : list) {
                 String[] split = tag.getAsString().split(" ");
-                BlockState state =level.getBlockState(this.getBlockPosBelowThatAffectsMyMovement().above().offset(Integer.parseInt(split[1]), Integer.parseInt(split[2]) + offset, Integer.parseInt(split[3])));
-                if (!state.isAir() && state.getBlock()!=(Blocks.WATER)) {
+                BlockState state = level.getBlockState(this.getBlockPosBelowThatAffectsMyMovement().above().offset(Integer.parseInt(split[1]), Integer.parseInt(split[2]) + offset, Integer.parseInt(split[3])));
+                if (!state.isAir() && state.getBlock() != (Blocks.WATER)) {
                     if (this.getControllingPassenger() instanceof Player) {
                         this.getControllingPassenger().sendMessage(new TextComponent("cannot disassemble, not enough space"), UUID.randomUUID());
 
@@ -182,19 +156,18 @@ public class BlockShipEntity extends Pig {
                     return;
                 }
             }
-            list.forEach(block->
+            list.forEach(block ->
             {
                 String[] split = block.getAsString().split(" ");
-                level.setBlockAndUpdate(this.getBlockPosBelowThatAffectsMyMovement().offset(Integer.parseInt(split[1]),Integer.parseInt(split[2])+offset+1,Integer.parseInt(split[3])), Block.stateById(Integer.parseInt(split[0])));
+                level.setBlockAndUpdate(this.getBlockPosBelowThatAffectsMyMovement().offset(Integer.parseInt(split[1]), Integer.parseInt(split[2]) + offset + 1, Integer.parseInt(split[3])), Block.stateById(Integer.parseInt(split[0])));
             });
-            storage.getAllKeys().forEach(blockEntity->
+            storage.getAllKeys().forEach(blockEntity ->
             {
                 String[] split = blockEntity.split(" ");
-                BlockPos newpos = this.getBlockPosBelowThatAffectsMyMovement().offset(Integer.parseInt(split[0]),Integer.parseInt(split[1])+offset+1,Integer.parseInt(split[2]));
+                BlockPos newpos = this.getBlockPosBelowThatAffectsMyMovement().offset(Integer.parseInt(split[0]), Integer.parseInt(split[1]) + offset + 1, Integer.parseInt(split[2]));
                 BlockEntity entity = level.getBlockEntity(newpos);
                 CompoundTag data = storage.getCompound(blockEntity);
-                if(data!=null)
-                {
+                if (data != null) {
                     data.putInt("x", newpos.getX());
                     data.putInt("y", newpos.getY());
                     data.putInt("z", newpos.getZ());
@@ -203,7 +176,7 @@ public class BlockShipEntity extends Pig {
                 }
             });
             this.ejectPassengers();
-            this.teleportToWithTicket(0,-1000,0);
+            this.teleportToWithTicket(0, -1000, 0);
         }
     }
 
@@ -219,19 +192,17 @@ public class BlockShipEntity extends Pig {
 
     @Override
     public boolean isControlledByLocalInstance() {
-        if(this.getItemBySlot(EquipmentSlot.HEAD).getItem()==Items.STICK)
-        {
-            return false;
-        }
+//        if(this.getItemBySlot(EquipmentSlot.HEAD).getItem()==Items.STICK)
+//        {
+//            return false;
+//        }
         return true;
     }
 
 
-
     @Override
     public InteractionResult mobInteract(Player p_230254_1_, InteractionHand p_230254_2_) {
-        if(!p_230254_1_.level.isClientSide)
-        {
+        if (!p_230254_1_.level.isClientSide) {
             p_230254_1_.startRiding(this);
         }
         return InteractionResult.SUCCESS;
@@ -239,13 +210,11 @@ public class BlockShipEntity extends Pig {
 
     @Override
     public InteractionResult interactAt(Player player, Vec3 p_184199_2_, InteractionHand hand) {
-        if(!player.getCommandSenderWorld().isClientSide && player.getItemInHand(hand)==ItemStack.EMPTY && hand==InteractionHand.MAIN_HAND)
-        {
+        if (!player.getCommandSenderWorld().isClientSide && player.getItemInHand(hand) == ItemStack.EMPTY && hand == InteractionHand.MAIN_HAND) {
             return InteractionResult.SUCCESS;
         }
-        if(!player.getCommandSenderWorld().isClientSide && player.getItemInHand(hand).getItem()== NyfsAirships.LIFT_JACK_ITEM && hand==InteractionHand.MAIN_HAND)
-        {
-            this.getItemBySlot(EquipmentSlot.CHEST).getTag().putInt("offset",player.getItemInHand(hand).getTag().getInt("off"));
+        if (!player.getCommandSenderWorld().isClientSide && player.getItemInHand(hand).getItem() == NyfsAirships.LIFT_JACK_ITEM && hand == InteractionHand.MAIN_HAND) {
+            this.getItemBySlot(EquipmentSlot.CHEST).getTag().putInt("offset", player.getItemInHand(hand).getTag().getInt("off"));
             return InteractionResult.SUCCESS;
         }
         return super.interactAt(player, p_184199_2_, hand);
@@ -254,24 +223,59 @@ public class BlockShipEntity extends Pig {
     @Override
     public void positionRider(Entity passenger) {
         super.positionRider(passenger);
-        passenger.fallDistance=0;
+        passenger.fallDistance = 0;
 
         int extra = 0;
-        if(this.getItemBySlot(EquipmentSlot.CHEST).getItem()==Items.OAK_PLANKS)
-        {
-            extra = this.getItemBySlot(EquipmentSlot.CHEST).getTag().getInt("offset")-1;
+        if (this.getItemBySlot(EquipmentSlot.CHEST).getItem() == Items.OAK_PLANKS) {
+            extra = this.getItemBySlot(EquipmentSlot.CHEST).getTag().getInt("offset") - 1;
         }
-        if(this.getControllingPassenger() instanceof Player)
-        {
-            if(passenger==this.getControllingPassenger())
-            {
-                passenger.setPos(this.getX(),this.getY()+0.5+extra,this.getZ());
+        if (this.getControllingPassenger() instanceof Player) {
+            if (passenger == this.getControllingPassenger()) {
+                passenger.setPos(this.getX(), this.getY() + 0.5 + extra, this.getZ());
             }
         }
     }
 
+    @Nullable
+    @Override
+    public Player getControllingPassenger() {
+        return (Player) getFirstPassenger();
+    }
+
+    @Override
+    public void travel(Vec3 pTravelVector) {
+        if (this.isAlive()) {
+            LivingEntity livingentity = this.getControllingPassenger();
+            if (this.isVehicle() && livingentity != null) {
+                this.fallDistance = 0;
+                this.setYRot(livingentity.getYRot());
+                this.yRotO = this.getYRot();
+                this.setXRot(livingentity.getXRot() * 0.5F);
+                this.setRot(this.getYRot(), this.getXRot());
+                this.yBodyRot = this.getYRot();
+                this.yHeadRot = this.yBodyRot;
+                float strafe = livingentity.xxa * 0.5F;
+                float forward = livingentity.zza;
+                if (forward <= 0.0F) {
+                    forward *= 0.25F;
+                }
 
 
+                this.flyingSpeed = this.getSpeed();
+                if (this.isControlledByLocalInstance()) {
+                    this.setSpeed((float) this.getAttributeValue(Attributes.MOVEMENT_SPEED));
+                    this.flyingSpeed = this.getSpeed();
+                    super.travel(new Vec3((double) strafe, pTravelVector.y, (double) forward));
+                } else if (livingentity instanceof Player) {
+                    this.setDeltaMovement(Vec3.ZERO);
+                }
+
+
+                this.calculateEntityAnimation(this, false);
+                this.tryCheckInsideBlocks();
+            }
+        }
+    }
 
 
     @Override
